2959a1a53d5fed92bb31ca103716496db7349e95
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-04-04 12:07:40 +0600

    dragon news-use-react-router-firebase-auth

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfwDT8NTo4BHjeBsU_XoZDfem64L_7zGY",
+  authDomain: "react-dragon-news-auth-4d140.firebaseapp.com",
+  projectId: "react-dragon-news-auth-4d140",
+  storageBucket: "react-dragon-news-auth-4d140.appspot.com",
+  messagingSenderId: "1042091035643",
+  appId: "1:1042091035643:web:199575989a1aefedfd195e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
